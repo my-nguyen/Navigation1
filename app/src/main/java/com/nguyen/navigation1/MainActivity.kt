43897cf5f7e8f26cffbf216172913e74862323bc
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1G.local>
2023-12-21 18:38:56 -0800

    b1: starter code

---
+++ app/src/main/java/com/nguyen/navigation1/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.nguyen.navigation1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.nguyen.navigation1.databinding.ActivityMainBinding
+
+/**
+ * Main Activity and entry point for the app. Displays a RecyclerView of letters.
+ */
+class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        recyclerView = binding.recyclerView
+        // Sets the LinearLayoutManager of the recyclerview
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = LetterAdapter()
+    }
+
+}
