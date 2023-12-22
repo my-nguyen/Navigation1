d8f7fb747e2ad8e006757b94834fcbd5f37bccc0
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1G.local>
2023-12-21 19:20:12 -0800

    b3: implement LetterListFragment

--- app/src/main/java/com/nguyen/navigation1/MainActivity.kt
+++ app/src/main/java/com/nguyen/navigation1/MainActivity.kt
@@ -10,18 +10,10 @@ import com.nguyen.navigation1.databinding.ActivityMainBinding
  * Main Activity and entry point for the app. Displays a RecyclerView of letters.
  */
 class MainActivity : AppCompatActivity() {
-    private lateinit var recyclerView: RecyclerView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        recyclerView = binding.recyclerView
-        // Sets the LinearLayoutManager of the recyclerview
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = LetterAdapter()
     }
-
 }
--- app/src/main/java/com/nguyen/navigation1/WordListFragment.kt
+++ app/src/main/java/com/nguyen/navigation1/WordListFragment.kt
@@ -1,6 +1,58 @@
 package com.nguyen.navigation1
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.nguyen.navigation1.databinding.FragmentWordListBinding
 
 class WordListFragment : Fragment(R.layout.fragment_word_list) {
+    private var _binding: FragmentWordListBinding? = null
+    private val binding get() = _binding!!
+    companion object {
+        const val LETTER = "letter"
+        const val SEARCH_PREFIX = "https://www.google.com/search?q="
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Retrieve a binding object that allows you to refer to views by id name
+        // Names are converted from snake case to camel case.
+        // For example, a View with the id word_one is referenced as binding.wordOne
+        _binding = FragmentWordListBinding.inflate(inflater, container, false)
+
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Retrieve the LETTER from the Intent extras
+        // intent.extras.getString returns String? (String or null)
+        // so toString() guarantees that the value will be a String
+        val letterId = requireActivity().intent?.extras?.getString(LETTER).toString()
+
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = WordAdapter(letterId, requireContext())
+
+        // Adds a [DividerItemDecoration] between items
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL)
+        )
+
+        // title = getString(R.string.detail_prefix) + " " + letterId
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
