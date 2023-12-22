269d4eb8897fb8547c95cd6a3491d75b8b422172
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1G.local>
2023-12-21 18:45:13 -0800

    b2: Create Fragment and Layout files

---
+++ app/src/main/java/com/nguyen/navigation1/LetterListFragment.kt
@@ -0,0 +1,7 @@
+package com.nguyen.navigation1
+
+import androidx.fragment.app.Fragment
+
+
+class LetterListFragment : Fragment(R.layout.fragment_letter_list) {
+}
---
+++ app/src/main/java/com/nguyen/navigation1/WordListFragment.kt
@@ -0,0 +1,6 @@
+package com.nguyen.navigation1
+
+import androidx.fragment.app.Fragment
+
+class WordListFragment : Fragment(R.layout.fragment_word_list) {
+}
